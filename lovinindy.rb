f106a1e9c50cdeab7b59d25d80c029fc8a35189a
gabroselli <gabroselli@gmail.com>
2011-09-17 12:56:41 -0400

    Gemfile

--- lovinindy.rb
+++ lovinindy.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'sinatra'
 require 'httparty'
 require 'embedly'
-require 'sinatra-cache'
 require 'twitter-text'
 
 include Twitter::Extractor
@@ -11,8 +10,10 @@ get '/' do
   @data = HTTParty.get("https://api.twitter.com/search.json?q=%23indy&page=1&rpp=20")['results']
   @data.each do |result|
     url = URI.extract(result, ['http'])
-    embedly_api = Embedly::API.new :key => 'a0254700e14811e08c704040d3dc5c07', :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; info@supermatter.com)'
-    obj = embedly_api.oembed :url => url
+    unless url.nil?
+      embedly_api = Embedly::API.new :key => 'a0254700e14811e08c704040d3dc5c07', :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; info@supermatter.com)'
+      obj = embedly_api.oembed :url => url[0]
+    end
   end
   erb :index
 end
