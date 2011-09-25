24ea2f93d4d45682ed8474f4d3365a1201930bd8
Gabriele Roselli <gabroselli@Gabriele-Rosellis-MacBook-Pro.local>
2011-09-25 14:16:55 -0400

    Caching page

--- lovin_indy.rb
+++ lovin_indy.rb
@@ -12,26 +12,22 @@ class LovinIndy < Sinatra::Base
   set :public, 'public'
 
   get '/' do
-    embedly_api = Embedly::API.new :key => 'a0254700e14811e08c704040d3dc5c07', :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; info@supermatter.com)'
-    if defined? time_stamp && time_stamp.nil? || time_stamp > Time.now + 300
+    if settings.cache.get('page').nil? || settings.cache.get('page')[:timestamp]+300 =< Time.now
+      embedly_api = Embedly::API.new :key => 'a0254700e14811e08c704040d3dc5c07', :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; info@supermatter.com)'
       @data = twitter_search.hashtag("indy").language("en").no_retweets.per_page(20)
-      time_stamp = Time.now
-      settings.cache.set('data', @data)
-    else
-      @data = settings.cache.get('data')
-    end
-    #@mentions = twitter_search.mentioning("lovinindy").per_page(10)
-    @data.each do |result|
-      url = URI.extract(result.text, ['http']).first
-      unless url.nil?
-        obj = embedly_api.oembed :url => url
-        result.embed_media_type = obj[0].type
-        result.embed_media_thumb = obj[0].thumbnail_url
-        result.embed_media_html = obj[0].html
-        result.embed_media_provider = obj[0].provider_name
+    
+      @data.each do |result|
+        url = URI.extract(result.text, ['http']).first
+        unless url.nil?
+          obj = embedly_api.oembed :url => url
+          result.embedly = obj[0]
+        end
       end
+      erb :index
+      settings.cache.set('view', {:page => erb :index, timestamp => Time.now})
+    else
+      settings.cache.get('page')[:page]
     end
-    erb :index
   end
 
   get '/search' do 
