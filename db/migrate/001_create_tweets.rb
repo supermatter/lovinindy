f6161570b6669996cce8ba51e88b684951697de1
Gabriele Roselli <groselli@igodigital.com>
2012-03-04 21:04:00 -0500

    Migration for tweets table

---
+++ db/migrate/001_create_tweets.rb
@@ -0,0 +1,16 @@
+class CreateTweets < ActiveRecord::Migration
+  def change
+    create_table :tweets do |t|
+      t.string :name
+			t.date :created_at
+			t.string :from_user
+			t.integer :from_user_id
+			t.integer :from_user_id_str
+			t.string :from_user_name
+			t.string :iso_language_code
+			t.string :profile_image_url
+			t.string :text
+      t.timestamps
+    end
+  end
+end
